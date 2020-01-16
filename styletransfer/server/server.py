9d6e4c85ecbd2789f1ea72b71b14f0f5ac2d81f6
adblock-filters <bartorbard@gmail.com>
2020-01-16 21:42:54 +0100

    fix

--- styletransfer/server/server.py
+++ styletransfer/server/server.py
@@ -6,7 +6,7 @@ import os
 import ssl
 import uuid
 
-import cv2
+# import cv2
 from aiohttp import web
 from av import VideoFrame
 
@@ -53,36 +53,36 @@ class VideoTransformTrack(MediaStreamTrack):
     async def recv(self):
         frame = await self.track.recv()
 
-        if self.transform == "cartoon":
-            img = frame.to_ndarray(format="bgr24")
-            print(type(img))
+        # if self.transform == "cartoon":
+        #     img = frame.to_ndarray(format="bgr24")
+        #     print(type(img))
             
-            # prepare color
-            img_color = cv2.pyrDown(cv2.pyrDown(img))
-            for _ in range(6):
-                img_color = cv2.bilateralFilter(img_color, 9, 9, 7)
-            img_color = cv2.pyrUp(cv2.pyrUp(img_color))
+        #     # prepare color
+        #     img_color = cv2.pyrDown(cv2.pyrDown(img))
+        #     for _ in range(6):
+        #         img_color = cv2.bilateralFilter(img_color, 9, 9, 7)
+        #     img_color = cv2.pyrUp(cv2.pyrUp(img_color))
 
-            # prepare edges
-            img_edges = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-            img_edges = cv2.adaptiveThreshold(
-                cv2.medianBlur(img_edges, 7),
-                255,
-                cv2.ADAPTIVE_THRESH_MEAN_C,
-                cv2.THRESH_BINARY,
-                9,
-                2,
-            )
-            img_edges = cv2.cvtColor(img_edges, cv2.COLOR_GRAY2RGB)
+        #     # prepare edges
+        #     img_edges = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        #     img_edges = cv2.adaptiveThreshold(
+        #         cv2.medianBlur(img_edges, 7),
+        #         255,
+        #         cv2.ADAPTIVE_THRESH_MEAN_C,
+        #         cv2.THRESH_BINARY,
+        #         9,
+        #         2,
+        #     )
+        #     img_edges = cv2.cvtColor(img_edges, cv2.COLOR_GRAY2RGB)
 
-            # combine color and edges
-            img = cv2.bitwise_and(img_color, img_edges)
+        #     # combine color and edges
+        #     img = cv2.bitwise_and(img_color, img_edges)
 
-            # rebuild a VideoFrame, preserving timing information
-            new_frame = VideoFrame.from_ndarray(img, format="bgr24")
-            new_frame.pts = frame.pts
-            new_frame.time_base = frame.time_base
-            return new_frame
+        #     # rebuild a VideoFrame, preserving timing information
+        #     new_frame = VideoFrame.from_ndarray(img, format="bgr24")
+        #     new_frame.pts = frame.pts
+        #     new_frame.time_base = frame.time_base
+        #     return new_frame
 
         if self.transform == "style":
             im = frame.to_ndarray(format="bgr24")
